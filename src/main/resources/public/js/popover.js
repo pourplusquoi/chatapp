a1060457801680dd9f64d78660a21159ac0bf141
Haoran Liang <harlan0103@gmail.com>
2018-11-24 19:25:24 -0600

    GUI changes

--- src/main/resources/public/js/popover.js
+++ src/main/resources/public/js/popover.js
@@ -3,32 +3,66 @@ $(function() {
         // Get the popover element
         var element = $(this);
         // Define the behavior of element
-        element.popover({
-            trigger: 'manul',
-            html: true,
-            title: element.attr('name'), // Chat room name
-            placement: 'right',
-            content: function(){
-                return chatRoomDetail();
-            }
-        // When user mouse enter, show the details
-        }).on("mouseenter", function(){
-            var _this = this;
-            $(this).popover("show");
-            $(this).siblings(".popover").on("mouseleave", function(){
-                $(_this).popover('hide');
+        console.log(element.attr('id'));
+        var id = element.attr('id');
+        
+        // Show the current user tag
+        if(id == "current-user"){
+            element.popover({
+                trigger: 'manul',
+                html: true,
+                title: element.attr('name'), // User name
+                placement: 'bottom',
+                content: function(){
+                    return userDetail();
+                }       
+            // When user mouse enter, show the details
+            }).on("mouseenter", function(){
+                var _this = this;
+                $(this).popover("show");
+                $(this).siblings(".popover").on("mouseleave", function(){
+                    $(_this).popover('hide');
+                });
+            // When user mouse leave, hide the details
+            }).on("mouseleave", function(){ 
+                var _this = this;
+                setTimeout(function(){
+                    if(!$(".popover:hover").length){
+                        $(_this).popover("hide")
+                    }
+                }, 100);
             });
-        // When user mouse leave, hide the details
-        }).on("mouseleave", function(){ 
-            var _this = this;
-            setTimeout(function(){
-                if(!$(".popover:hover").length){
-                    $(_this).popover("hide")
-                }
-            }, 100);
-        });
+        }
+        // Show chat room tag
+        else{
+            element.popover({
+                trigger: 'manul',
+                html: true,
+                title: element.attr('name'), // Chat room name
+                placement: 'right',
+                content: function(){
+                    return chatRoomDetail();
+                }            
+            // When user mouse enter, show the details
+            }).on("mouseenter", function(){
+                var _this = this;
+                $(this).popover("show");
+                $(this).siblings(".popover").on("mouseleave", function(){
+                    $(_this).popover('hide');
+                });
+            // When user mouse leave, hide the details
+            }).on("mouseleave", function(){ 
+                var _this = this;
+                setTimeout(function(){
+                    if(!$(".popover:hover").length){
+                        $(_this).popover("hide")
+                    }
+                }, 100);
+            });
+        }
     });
 });  
+
 // The chat room detail interface
 function chatRoomDetail(){      
     var data = $(
@@ -40,6 +74,20 @@ function chatRoomDetail(){
             "</ul>" +
                     
             "<input id='btn' type='button' value='EXIT' onclick='exit()' class='btn btn-primary'/>" +
+        "</div>"
+    );            
+    return data;  
+}  
+
+// The user tag interface
+function userDetail(){      
+    var data = $(
+        "<div class='chat-room-details'>" + 
+            "<ul>" + 
+                "<li><span><img src='static/img/age.png'>Age</span></li>" +                
+                "<li><span><img src='static/img/location.png'>North America</span></li>" +
+                "<li><span><img src='static/img/university.png'>Rice University</span></li>" +
+            "</ul>" +
         "</div>"
     );            
     return data;  
