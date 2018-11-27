058711ec966309d7f2250e54276cdd46dd7b5342
Jialei Zhou <jialei.loy.zhou@gmail.com>
2018-11-27 02:35:35 -0800

    delete all

--- src/main/resources/public/js/main.js
+++
@@ -1,7 +0,0 @@
-// window.onload = function(){
-//     var loginDiv = document.getElementById("login-space");
-//     var mainDiv = document.getElementById("whole-page");
-//     // 设置login为可视 main为隐藏
-//     loginDiv.style.display = "block";
-//     mainDiv.style.display = "none";
-// }
